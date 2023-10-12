49de70b498eed523d4a35099ea078add0cbf93f1
Kenneth Adutwum <kwamayadu@gmail.com>
2023-10-11 21:48:50 -0700

    Error corrected

--- src/component/CreateBook.jsx
+++ src/component/CreateBook.jsx
@@ -27,8 +27,7 @@ const CreateBook = () => {
       <h2>Add a New Book</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          {/* <label htmlFor="title">Title:</label> */}
-          <input
+         <input
             type="text"
             id="title"
             placeholder="Title"
@@ -38,7 +37,6 @@ const CreateBook = () => {
           />
         </div>
         <div>
-          {/* <label htmlFor="title">Title:</label> */}
           <input
             type="text"
             id="author"
@@ -49,7 +47,6 @@ const CreateBook = () => {
           />
         </div>
         <div>
-          {/* <label htmlFor="author">Author:</label> */}
           <select className="category" onChange={handleAuthorChange} id="color" name="category">
             <option value={author}>Category</option>
             <option value="green">Action</option>
